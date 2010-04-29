bb2b94866e58594ddb5cf2dece958aef26740ba5
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2010-04-28 19:26:15 -0700

    Removed unnecessary code

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,12 +2,7 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-	protected function _initDoctype()
-	{
-		$this->bootstrap('view');
-		$view = $this->getResource('view');
-		$view->doctype('XHTML1_TRANSITIONAL');
-	}
+    /* NULL */
 }
 
 require_once 'Zend/Controller/Front.php';
